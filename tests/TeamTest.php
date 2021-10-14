daa89f4dba6771d8500888da7e834b8ade1fecb0
green68 <eric.vercheval@hotmail.fr>
2021-10-14 13:20:26 +0200

    first commit

---
+++ tests/TeamTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+use PHPUnit\Framework;
+use PHPUnit\Framework\TestCase;
+
+class TeamTest extends TestCase
+{
+
+    public function testDummyFunction(){
+
+        $this->assertTrue(true);
+    }
+}
